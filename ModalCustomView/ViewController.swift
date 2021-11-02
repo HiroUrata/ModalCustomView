40178c5c850029087aa6699a4afd46843cb33dc9
HiroUrata <1712026@i.softbank.jp>
2021-11-02 17:18:53 +0900

    ハーフモーダルの Viewを作成　

--- ModalCustomView/ViewController.swift
+++ ModalCustomView/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+       let showModalViewButton = UIButton()
+        showModalViewButton.frame = CGRect(x: view.frame.maxX / 4, y: 100, width: view.frame.width / 2, height: 40)
+        showModalViewButton.backgroundColor = .systemGreen
+        showModalViewButton.setTitle("showModalView", for: .normal)
+        showModalViewButton.titleLabel?.textColor = .black
+        showModalViewButton.addTarget(self, action: #selector(showView), for: .touchDown)
+        view.addSubview(showModalViewButton)
+    }
+    
+    @objc func showView(){
+        
+        let modalVC = ModalViewController()
+        modalVC.modalPresentationStyle = .custom
+        self.present(modalVC, animated: true, completion: nil)
     }
 
+    @objc func dismissController(){
+        
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
